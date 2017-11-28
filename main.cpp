dbc774d7d6bae70769f0cf53ddab001253caaa7b
Sergio Tenza <PROGRAMACION@NEXUS>
2017-11-28 09:21:58 +0100

    Cambiado main con codigo de Esteban

--- main.cpp
+++ main.cpp
@@ -11,15 +11,53 @@
  * Created on 21 de noviembre de 2017, 12:43
  */
 
-#include <cstdlib>
 
-using namespace std;
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /*
  * 
  */
-int main(int argc, char** argv) {
-
-    return 0;
+//int comprobante(int z, char p[]);
+int main() {
+    int numerico;
+    char numero[9];
+    int control;
+    do
+   {
+       system("CLEAR");
+        printf("Seleccione la opción correspondiente a la aplicación que vaya a usar\n1. Obtención letra DNI\n2. Cálculo de números romanos\n0. Finalizar\n");
+        scanf("%i",&numerico);
+        if (numerico == 1 || numerico == 2)
+        {
+        printf("Indique el numero a transformar\n");
+        scanf("%s", &numero);
+        }
+        switch(numerico)
+        {
+            case 1 :
+                //comprobante(numerico,numero);
+                break;
+            case 2 :
+                //comprobante(numerico,numero);
+                break;
+            
+            case 0 :
+                printf("Programa finalizado");
+                break;
+              
+                
+            default :
+                printf("Número erroneo");
+                break;
+        }
+        if (numerico == 0)
+        {
+            break;
+        }
+        printf("\n\n¿Quiere continuar?\n1.Si\n2.No\n");
+        scanf("%i",&control);
+    }while (control == 1);
+    
 }
-
